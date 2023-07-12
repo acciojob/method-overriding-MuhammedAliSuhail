7144d40d84ef2868b1848cb1425974e1bb1c787d
unknown <suhailali3454@gmail.com>
2023-07-12 17:40:36 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
-public class Main {
-  
+
+public  class Main {
+    public static class A{
+
+public String  meth(){
+   // System.out.println("A");
+    return "Invoking method from class A";
+}
+    }
+
+    public static class B extends A{
+        public String  meth(){
+          //  System.out.println("B");
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String args[]){
+        B obj = new B();
+        obj.meth();
+        B obj1= new B();
+        obj1.meth();
+    }
 }
